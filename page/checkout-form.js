9784ca5f46290b1a0c007c8160447b956a001589
Mostafa Ibrahim <mnmaibrahim@gmail.com>
2026-08-13 10:19:19 +0300

    GG

--- page/checkout-form.js
+++ page/checkout-form.js
@@ -119,6 +119,7 @@
     { id: "architectureInput", errorId: "architectureError", label: "Building NO. is required." },
     { id: "cardholderInput", errorId: "cardholderError", label: "Cardholder name is required." },
     { id: "floorInput", errorId: "FloorError", label: "Floor is required." },
+    { id: "phoneInput", errorId: "phoneError", label: "Phone is required." },
     { id: "checkoutEmail", errorId: "emailError", label: "Email is required." },  
   ];
 
