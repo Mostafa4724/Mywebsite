664266a34c9098418c94c632061e03f67d75605f
Mostafa Ibrahim <mnmaibrahim@gmail.com>
2026-08-15 10:26:31 +0300

    jbv

--- admin/view-order.js
+++ admin/view-order.js
@@ -1365,9 +1365,6 @@
 
         renderStatusControl();
 
-        // Refresh the order bubble immediately after the status changes.
-        window.dispatchEvent(new Event("orderStatusUpdated"));
-
 
         showToast(
           "Order marked as " +
