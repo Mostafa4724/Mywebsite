46e7d48959d66a1f7b875ad350e65a41b7faac2d
Mostafa Ibrahim <mnmaibrahim@gmail.com>
2026-08-15 22:53:49 +0300

    xxx

--- page/contact.js
+++ page/contact.js
@@ -1,9 +1,9 @@
 (function () {
   "use strict";
 
-  var PUBLIC_KEY  = "POyuLcs-3SnIXAd8u";
-  var SERVICE_ID  = "service_o3h0CDF";
-  var TEMPLATE_ID = "template_ql3kd9w";
+  var EMAILJS_PUBLIC_KEY  = "POyuLcs-3SnIXAd8u";
+  var EMAILJS_SERVICE_ID  = "service_ozjcqmp";
+  var EMAILJS_TEMPLATE_ID = "template_ql3kd9w";
 
   var form      = document.getElementById("contactForm");
   var submitBtn = document.getElementById("contactSubmitBtn");
@@ -24,26 +24,7 @@
     submitBtn.parentNode.insertBefore(el, submitBtn.nextSibling);
   }
 
-  // Verify EmailJS loaded
-  if (typeof emailjs === "undefined") {
-    console.error("EmailJS library not loaded!");
-    return;
-  }
-
-  emailjs.init(PUBLIC_KEY);
-
-  // Quick account verification
-  console.log("EmailJS initialized. Testing connection...");
-  emailjs.send(SERVICE_ID, TEMPLATE_ID, {
-    from_name: "Test",
-    from_email: "test@test.com",
-    to_email: "mnmaibrahim@gmail.com",
-    message: "Connection test - ignore this email"
-  }).then(function() {
-    console.log("Connection test PASSED!");
-  }).catch(function(err) {
-    console.error("Connection test FAILED:", err.status, err.text);
-  });
+  emailjs.init(EMAILJS_PUBLIC_KEY);
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -63,7 +44,7 @@
     submitBtn.textContent = "Sending...";
 
     emailjs
-      .send(SERVICE_ID, TEMPLATE_ID, {
+      .send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
         from_name:  name,
         from_email: email,
         to_email:   "mnmaibrahim@gmail.com",
